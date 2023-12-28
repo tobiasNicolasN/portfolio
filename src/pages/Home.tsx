40f4033f05997b389b6fdc903db700f2e4a599f3
Tobias <tobias.nicolas001@gmail.com>
2023-12-27 23:56:11 -0300

    Styling the Home Page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,10 +5,8 @@ function Home() {
   return (
     <body>
       <div className={styles.container}>
-        <div>
           <h1>Tobias Nicolas Nuñez</h1>
           <h2>FullStack Developer</h2>
-        </div>
         <div className={styles.buttons}>
           <Navigate path="/en" buttonText="English" />
           <Navigate path="/es" buttonText="Español" />
