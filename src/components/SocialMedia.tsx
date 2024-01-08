fcb3e3b4cb513190de8d1800814279db99e73c7d
Tobias <tobias.nicolas001@gmail.com>
2024-01-08 10:04:45 -0300

    Styling Home Page
    SocialMedia and nav buttons finished

---
+++ src/components/SocialMedia.tsx
@@ -0,0 +1,27 @@
+import { FaGithubSquare, FaLinkedin } from "react-icons/fa";
+import style from "./SocialMedia.module.css";
+
+function SocialMedia() {
+  return (
+    <>
+      <a
+        className={style.button}
+        href="https://github.com/tobiasNicolasN"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FaGithubSquare />
+      </a>
+      <a
+        className={style.button}
+        href="https://www.linkedin.com/in/tobiasnicolasn/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FaLinkedin />
+      </a>
+    </>
+  );
+}
+
+export default SocialMedia;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,19 @@
 import Navigate from "../components/Navigate";
 import styles from "./Home.module.css";
+import SocialMedia from '../components/SocialMedia'
 
 function Home() {
   return (
     <body>
       <div className={styles.container}>
-          <h1>Tobias Nicolas Nuñez</h1>
-          <h2>FullStack Developer</h2>
+        <h1>Tobias Nicolas Nuñez</h1>
+        <h2>FullStack Developer</h2>
         <div className={styles.buttons}>
           <Navigate path="/en" buttonText="English" />
           <Navigate path="/es" buttonText="Español" />
+        </div>
+        <div className={styles.social}>
+          <SocialMedia />
         </div>
       </div>
     </body>
