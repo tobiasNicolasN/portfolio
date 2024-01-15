bb4532d8919e0367a157562cba4b6ed00cf9b816
Tobias <tobias.nicolas001@gmail.com>
2024-01-15 18:08:16 -0300

    Styling Portfolio
    Adding information

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -15,9 +15,9 @@ function Español({ language, changeLanguage }: IPorfolio) {
         <>
           <NavBar language={language} />
           <div className={style.aboutContainer}>
-            <p className={style.about}>
+            <p className={style.text}>
               Soy un{" "}
-              <span className={style.colorText}>Desarrollador FullStack</span>{" "}
+              <span className={style.highlighted}>Desarrollador FullStack</span>{" "}
               de veintitrés años. Me esfuerzo por crear soluciones tecnológicas
               que mejoren la vida de las personas. <br />
               <br /> Empezar a desarrollar cambió mi perspectiva, impulsándome a
@@ -27,17 +27,30 @@ function Español({ language, changeLanguage }: IPorfolio) {
               <br /> Busco colaborar en equipos dinámicos donde pueda
               enriquecerme y contribuir de manera significativa. <br />
               <br /> Actualmente, resido en{" "}
-              <span className={style.colorText}>Buenos Aires, Argentina.</span>
+              <span className={style.highlighted}>
+                Buenos Aires, Argentina.
+              </span>
             </p>
           </div>
           <hr />
           <div className={style.skillsContainer}>
-              <p>Front-End</p>
-            <div>
-              <p>REACT</p>
-              <p>HTML</p>
-              <p>CSS</p>
-              
+            <p className={style.titleSkill}>Habilidades</p>
+            <hr />
+            <div className={style.skills}>
+              <p className={style.skillSection}>Frontend</p>
+              <p className={style.skillText}>
+                React, TypeScript, JavaScript, Html, Css.
+              </p>
+            </div>
+            <div className={style.skills}>
+            <p className={style.skillSection}>Backend y Servidores</p>
+              <p className={style.skillText}>Express, Node.js</p>
+            </div>
+            <div className={style.skills}>
+            <p className={style.skillSection}>Bases de Datos y Herramientas</p>
+              <p className={style.skillText}>
+                MongoDB, MySQL, Git, GitHub, Vite
+              </p>
             </div>
           </div>
           <button
@@ -51,17 +64,45 @@ function Español({ language, changeLanguage }: IPorfolio) {
         <>
           <NavBar language={language} />
           <div className={style.aboutContainer}>
-            <p className={style.about}>
-              I'm twenty-three year old {" "}
-              <span className={style.colorText}>FullStack Developer.</span>{" "}
-              I strive to create technological solutions that improve people's lives. <br />
-              <br /> Starting to develop changed my perspective, driving me to maximize each project with a constant focus on quality. I am passionate about learning new technologies and exploring creative solutions. <br />
-              <br /> I seek to collaborate in dynamic teams where I can enrich myself and contribute significantly. <br />
+            <p className={style.text}>
+              I'm twenty-three year old{" "}
+              <span className={style.highlighted}>FullStack Developer.</span> I
+              strive to create technological solutions that improve people's
+              lives. <br />
+              <br /> Starting to develop changed my perspective, driving me to
+              maximize each project with a constant focus on quality. I am
+              passionate about learning new technologies and exploring creative
+              solutions. <br />
+              <br /> I seek to collaborate in dynamic teams where I can enrich
+              myself and contribute significantly. <br />
               <br /> Currently living in{" "}
-              <span className={style.colorText}>Buenos Aires, Argentina.</span>
+              <span className={style.highlighted}>
+                Buenos Aires, Argentina.
+              </span>
             </p>
           </div>
           <hr />
+          <div className={style.skillsContainer}>
+            <p className={style.titleSkill}>Skills</p>
+            <hr />
+            <div className={style.skills}>
+              <p className={style.skillSection}>Frontend</p>
+              <p className={style.skillText}>
+                React, TypeScript, JavaScript, Html, Css.
+              </p>
+            </div>
+            <div className={style.skills}>
+            <p className={style.skillSection}>Backend and Servers</p>
+              <p className={style.skillText}>Express, Node.js</p>
+            </div>
+            <div className={style.skills}>
+            <p className={style.skillSection}>Databases and Tools</p>
+              <p className={style.skillText}>
+                MongoDB, MySQL, Git, GitHub, Vite
+              </p>
+            </div>
+          </div>
+
           <button
             className={style.languageButton}
             onClick={() => changeLanguage(language)}
